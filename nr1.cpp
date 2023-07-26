ea2336c8411dc06afce7b4b0ab8c413b758c6ada
tschoemaker <=>
2023-07-26 22:21:03 +0200

    added part2

--- nr1.cpp
+++ nr1.cpp
@@ -39,14 +39,25 @@ unsigned int get_max_sum(std::vector<Elf> elves){
     }
     return max;
 }
+bool comp(Elf e1,Elf e2){
+    return e2.sum()<e1.sum();
+}
 int main(int argc,const char** argv){
     if(argc<=1){
         std::cerr<<"No path provided"<<std::endl;
         return -1;
     }
+    bool task2=argc>=3;
     std::string path=argv[1];
     std::vector<Elf> elves;
     load_input(elves,path);
-    std::cout<<get_max_sum(elves)<<std::endl;
+    std::sort(elves.begin(),elves.end(),comp);
+
+    int n=task2?3:1;
+    int s=0;
+    for(int i=0;i<n;i++){
+        s+=elves[i].sum();
+    }
+    std::cout<<s<<std::endl;
     
 }
